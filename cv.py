dba3609688f6b392eb3b48d7aa2cef836f6ea248
PythonNut <PythonNut@users.noreply.github.com>
2018-11-03 00:40:20 -0700

    Now the face points are well-ordered!

--- cv.py
+++ cv.py
@@ -22,9 +22,21 @@ def as_u8(mat):
 def query_ball_midpoint(kd, c1, c2):
     return kd.query_ball_point((c1 + c2)/2, 10)
 
+def angle_relative_to(center, mapping):
+    def angle_closure(index):
+        x, y = mapping[index] - mapping[center]
+        angle = -np.arctan2(y, x)
+        if angle < 0:
+            angle += 2 * np.pi
+        return angle
+    return angle_closure
+
+def rotate(l, n):
+    return l[n:] + l[:n]
 
 while True:
     ret_val, img = cam.read()
+    cv2.imshow("input", img)
     img = cv2.GaussianBlur(img,(5,5),0)
     # lap = cv2.Laplacian(img,cv2.CV_64F)
     sobelx = cv2.Sobel(img,cv2.CV_64F,1,0,ksize=5)
@@ -36,6 +48,7 @@ while True:
     s = s > 1
     s = s.astype('uint8')
     s = 1 - s
+    cv2.imshow("connected regions", s*255)
     # s *= 255
     # s = iterative_refine(s, 1)
     # s2 = cv2.threshold(s, 127, 255, cv2.THRESH_BINARY)[1]
@@ -49,6 +62,7 @@ while True:
     s3 = np.zeros(s.shape + (3,), np.uint8)
     num_labels, labels, stats, centroids = connected
     interesting_centroids = []
+    interesting_centroid_labels = []
     for label in range(num_labels):
         centroid = centroids[label]
         area = stats[label, cv2.CC_STAT_AREA]
@@ -66,11 +80,14 @@ while True:
 
         s2 += labels == label
         interesting_centroids.append(centroid)
+        interesting_centroid_labels.append(label)
 
         # contours,hierarchy,_ = cv2.findContours(as_u8(labels == label), 1, 2)
         # print(contours)
         # cv2.drawContours(s2, contours, -1, (0,255,0), 3)
         # print(area)
+
+    cv2.imshow("patches", s2)
 
     # print(interesting_centroids)
     corner_count = [0 for _ in range(len(interesting_centroids))]
@@ -80,6 +97,7 @@ while True:
     if not interesting_centroids:
         print("No interesting centroids!")
         continue
+
     kd = scipy.spatial.cKDTree(np.vstack(interesting_centroids))
     for i, c1 in enumerate(interesting_centroids):
         for j, c2 in enumerate(interesting_centroids):
@@ -91,18 +109,19 @@ while True:
     # print(corner_count)
 
     corners = []
-    for i, centroid in enumerate(interesting_centroids):
+    for i in range(len(interesting_centroids)):
         if corner_count[i] == 3:
-            corners.append(centroid)
+            corners.append(i)
 
     if len(corners) != 4:
         print("Need exactly four corners!")
 
     min_corner_dist = 1e8
     max_corner_dist = 1e-8
-    for i, c1 in enumerate(corners):
-        for j, c2 in enumerate(corners):
+    for i in corners:
+        for j in corners:
             if j >= i: continue
+            c1, c2 = interesting_centroids[i], interesting_centroids[j]
             dist = np.linalg.norm(c1 - c2)
             if dist > max_corner_dist:
                 max_corner_dist = dist
@@ -121,50 +140,50 @@ while True:
         #     color = (255, 0, 0)
         cv2.circle(s3, (int(centroid[0]), int(centroid[1])), 10, color, 4)
 
-    nine_point_indices = set()
-    nine_points = copy.deepcopy(corners)
+    nine_points = set(corners)
     center = None
-    for i, c1 in enumerate(corners):
-        for j, c2 in enumerate(corners):
+    for i in corners:
+        for j in corners:
             if j >= i: continue
+            c1, c2 = interesting_centroids[i], interesting_centroids[j]
             midpoints = query_ball_midpoint(kd, c1, c2)
             if len(midpoints) != 1: continue
             midpoint_idx = midpoints[0]
-            midpoint = interesting_centroids[midpoint_idx]
-            if midpoint_idx not in nine_point_indices:
-                nine_points.append(midpoint)
-                nine_point_indices.add(midpoint_idx)
+            if midpoint_idx not in nine_points:
+                nine_points.add(midpoint_idx)
             else:
-                center = midpoint
+                center = midpoint_idx
 
     if len(nine_points) != 9:
         print(f"9 point set has {len(nine_points)} points!")
-    print(nine_points)
 
+    if center is None:
+        print("Failed to find center of face!")
+    else:
+        eight_points = list(nine_points - set([center]))
+        # print(interesting_centroids[center], [interesting_centroids[p] for p in  eight_points])
+        print([angle_relative_to(center, interesting_centroids)(e) for e in eight_points])
+        eight_points.sort(key=angle_relative_to(center, interesting_centroids))
 
+        if eight_points[0] not in corners:
+            eight_points = rotate(eight_points, 1)
 
-    # for l in lines:
-    #     x0, y0, x1, y1 = l.flatten()
-    #     length = np.linalg.norm([x0 - x1, y0 - y1])
-    #     if length < 10:
-    #         continue
-    #     if length > 100:
-    #         continue
-    #     # print(length)
-    #     cv2.line(s2, (x0, y0), (x1,y1), 255, 1, cv2.LINE_AA)
-    # lines = cv2.HoughLinesP(s,1,np.pi/45,100,10,5)
+        print(eight_points)
 
-    # # print(lines)
-    # for line in lines:
-    #     for x1,y1,x2,y2 in line:
-    #         cv2.line(s2, (x1,y1),(x2,y2),(255,255,255),2)
+        if len(eight_points) == 8:
+            first_point = interesting_centroids[eight_points[0]]
+            cv2.circle(s3, (int(first_point[0]), int(first_point[1])), 3, (0,0,255), 4)
+
+            first_point = interesting_centroids[eight_points[1]]
+            cv2.circle(s3, (int(first_point[0]), int(first_point[1])), 3, (0,255,255), 4)
+
+            first_point = interesting_centroids[center]
+            cv2.circle(s3, (int(first_point[0]), int(first_point[1])), 3, (255,0,0), 4)
 
     # s /= np.ndarray(s).max()
     # cv2.normalize(s,  s2, 0, 255, cv2.NORM_MINMAX)
     # s2 = s.convertTo(cs2.CV_8UC1)
     cv2.imshow("output", s3)
-    cv2.imshow("patches", s2)
-    cv2.imshow("output2", s*255)
     # cv2.imshow("output", (connected[1] > 0).astype('uint8')*255)
     if cv2.waitKey(1) == 27:
         break
