92b354804aacc3094f7e29b7c43b29eeed370843
PythonNut <PythonNut@users.noreply.github.com>
2018-11-03 02:36:55 -0700

    We can now classify colors!

--- cv.py
+++ cv.py
@@ -4,6 +4,9 @@ import scipy.spatial
 from skimage import img_as_ubyte
 import collections
 import copy
+import time
+import shelve
+from pathlib import Path
 
 # from pylsd.lsd import lsd
 
@@ -34,10 +37,71 @@ def angle_relative_to(center, mapping):
 def rotate(l, n):
     return l[n:] + l[:n]
 
+def cvtPixel(color, conversion):
+    return cv2.cvtColor(np.array([[color]]).astype('uint8'), conversion)[0,0]
+
+def hue_delta(h1, h2):
+    d = abs(h1- h2)
+    return min(d, 180 - d)
+
+def hsv_metric(c1, c2):
+    h1, s1, v1 = c1.astype('float')
+    h2, s2, v2 = c2.astype('float')
+    return hue_delta(h1, h2)**2 + (abs(s1 - s2)/4)**2 + (abs(v1 - v2)/2)**2
+
+CALIBRATION_ORDER = "RWBYGO"
+CALIBRATION_INDEX = 0
+CALIBRATION_READY = False
+CALIBRATION_START_TIME = 0
+
+COLOR_MAP = {}
+DISPLAY_COLOR_MAP = {
+    "X": (0,0,0),
+    "R": (0,0,255),
+    "W": (255,255,255),
+    "B": (255,0,0),
+    "Y": (0,255,255),
+    "G": (0,255,0),
+    "O": (0,165,255)
+}
+
+def guess_color(c):
+    min_dist = float('inf')
+    min_name = "X"
+    for name, color in COLOR_MAP.items():
+        dist = hsv_metric(c, color)
+        if dist < min_dist:
+            min_dist = dist
+            min_name = name
+
+    return min_name
+
+def stochastic_pixel_sampler(img, x, y, radius=10, samples=100):
+    accumulator = np.zeros(3)
+    for _ in range(samples):
+        dx, dy = float('inf'), float('inf')
+        while dx**2 + dy**2 > radius**2:
+            dx = np.random.rand() * radius
+            dy = np.random.rand() * radius
+        accumulator += img[int(y + dy), int(x + dx)]
+    return accumulator / samples
+
+def draw_circle(img, p, r, c, t = 4):
+    cv2.circle(img, (int(p[0]), int(p[1])), r, c, t)
+
+
+
 while True:
+    # print(COLOR_MAP)
+    if CALIBRATION_INDEX < len(CALIBRATION_ORDER) and not CALIBRATION_READY:
+        side = CALIBRATION_ORDER[CALIBRATION_INDEX]
+        input(f"Please face side {side} > ")
+        CALIBRATION_READY = True
+        CALIBRATION_START_TIME = time.process_time()
+
     ret_val, img = cam.read()
-    cv2.imshow("input", img)
     img = cv2.GaussianBlur(img,(5,5),0)
+    cv2.imshow("input", img)
     # lap = cv2.Laplacian(img,cv2.CV_64F)
     sobelx = cv2.Sobel(img,cv2.CV_64F,1,0,ksize=5)
     sobely = cv2.Sobel(img,cv2.CV_64F,0,1,ksize=5)
@@ -138,7 +202,8 @@ while True:
             color = (255, 0, 255)
         # if corner_count[i] == 1:
         #     color = (255, 0, 0)
-        cv2.circle(s3, (int(centroid[0]), int(centroid[1])), 10, color, 4)
+        draw_circle(s3, centroid, 10, color, 4)
+        # cv2.circle(s3, (int(centroid[0]), int(centroid[1])), 10, color, 4)
 
     nine_points = set(corners)
     center = None
@@ -154,6 +219,7 @@ while True:
             else:
                 center = midpoint_idx
 
+    s4 = np.zeros(s.shape + (3,), np.uint8)
     if len(nine_points) != 9:
         print(f"9 point set has {len(nine_points)} points!")
 
@@ -162,28 +228,70 @@ while True:
     else:
         eight_points = list(nine_points - set([center]))
         # print(interesting_centroids[center], [interesting_centroids[p] for p in  eight_points])
-        print([angle_relative_to(center, interesting_centroids)(e) for e in eight_points])
+        # print([angle_relative_to(center, interesting_centroids)(e) for e in eight_points])
         eight_points.sort(key=angle_relative_to(center, interesting_centroids))
 
         if eight_points[0] not in corners:
             eight_points = rotate(eight_points, 1)
 
-        print(eight_points)
+        # print(eight_points)
 
         if len(eight_points) == 8:
             first_point = interesting_centroids[eight_points[0]]
-            cv2.circle(s3, (int(first_point[0]), int(first_point[1])), 3, (0,0,255), 4)
+            draw_circle(s3, first_point, 3, (0,0,255), 4)
+            # cv2.circle(s3, (int(first_point[0]), int(first_point[1])), 3, (0,0,255), 4)
 
             first_point = interesting_centroids[eight_points[1]]
-            cv2.circle(s3, (int(first_point[0]), int(first_point[1])), 3, (0,255,255), 4)
+            draw_circle(s3, first_point, 3, (0,255,255), 4)
+            # cv2.circle(s3, (int(first_point[0]), int(first_point[1])), 3, (0,255,255), 4)
 
             first_point = interesting_centroids[center]
-            cv2.circle(s3, (int(first_point[0]), int(first_point[1])), 3, (255,0,0), 4)
+            draw_circle(s3, first_point, 3, (255,0,0), 4)
+            # cv2.circle(s3, (int(first_point[0]), int(first_point[1])), 3, (255,0,0), 4)
+
+            rgb_color = stochastic_pixel_sampler(img, first_point[0], first_point[1])
+            hsv_color = cvtPixel(rgb_color, cv2.COLOR_BGR2HSV)
+            color_name = guess_color(hsv_color)
+            draw_circle(s4, first_point, 14, DISPLAY_COLOR_MAP[color_name], 4)
+            print(f"{color_name}", end="")
+
+            for i in eight_points:
+                point = interesting_centroids[i]
+                rgb_color = stochastic_pixel_sampler(img, point[0], point[1])
+                hsv_color = cvtPixel(rgb_color, cv2.COLOR_BGR2HSV)
+                color_name = guess_color(hsv_color)
+
+                draw_circle(s4, point, 16, DISPLAY_COLOR_MAP[color_name], 4)
+
+                # print(hsv_color)
+                print(f"{color_name}", end="")
+            print()
+
+            # Wow, we did it
+            if CALIBRATION_READY and time.process_time() - CALIBRATION_START_TIME > 1:
+                color = CALIBRATION_ORDER[CALIBRATION_INDEX]
+                color_acc = np.zeros(3)
+                for i in nine_points:
+                    point = interesting_centroids[i]
+                    color_acc += stochastic_pixel_sampler(img, point[0], point[1])
+                    # color_acc += img[int(point[0]), int(point[1])]
+
+                color_acc /= 9
+                hsv_color = cvtPixel(color_acc, cv2.COLOR_BGR2HSV)
+                COLOR_MAP[color] = hsv_color
+
+                print("gottem")
+
+                CALIBRATION_INDEX += 1
+                CALIBRATION_READY = False
+
+
 
     # s /= np.ndarray(s).max()
     # cv2.normalize(s,  s2, 0, 255, cv2.NORM_MINMAX)
     # s2 = s.convertTo(cs2.CV_8UC1)
     cv2.imshow("output", s3)
+    cv2.imshow("colors", s4)
     # cv2.imshow("output", (connected[1] > 0).astype('uint8')*255)
     if cv2.waitKey(1) == 27:
         break
